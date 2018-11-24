af9398aab0d4eec48163809d2cae1374556b5c01
thuliu-yt16 <liu-yt16@mails.tsinghua.edu.cn>
2018-11-24 21:50:35 +0800

    fake rom

--- src/inst_rom.vhd
+++ src/inst_rom.vhd
@@ -5,12 +5,25 @@ use ieee.numeric_std.all;
 -- input ports: addr, ce;
 -- output ports: inst;
 entity inst_rom is
-  port(addr_i: in std_logic_vector(15 downto 0);
-       ce_i: in std_logic;
+  port(addr_i: in std_logic_vector(15 downto 0); -- 要取指令的地址
+       ce_i: in std_logic; -- 芯片使能
 
-       inst_o: out std_logic_vector(15 downto 0));
+       inst_o: out std_logic_vector(15 downto 0)); -- 所取指令
 end inst_rom;
 
 architecture bhv of inst_rom is
-  begin
+type InstructionArray is array(0 to 1000) of std_logic_vector(15 downto 0);
+signal insts: InstructionArray := (
+     -- 写一些可供测试的指令
+     others => NopInst
+    );
+ begin
+    READ_INST: process(ce_i, addr_i)
+    begin
+        if(ce_i = Enable) then
+            inst_o <= insts(addr_i);
+        else
+            inst_o <= NopInst;
+        end if;
+    end process;
 end bhv;
